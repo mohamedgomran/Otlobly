368781cb6d35ce25f3e1e82242f9e9e9fb120dd0
Mohamed-Hasan <mohamed.hassan.pet@gmail.com>
2018-01-25 20:16:37 +0200

    modified database

--- cafeteria.sql
+++ cafeteria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 19, 2018 at 08:57 PM
+-- Generation Time: Jan 25, 2018 at 06:14 PM
 -- Server version: 5.5.56-MariaDB
 -- PHP Version: 5.4.16
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Cafeteria`
+-- Database: `cafeteria`
 --
 
 DELIMITER $$
@@ -27,25 +27,25 @@ DELIMITER $$
 CREATE DEFINER=`mhassan`@`localhost` PROCEDURE `checks`(IN `startDate` DATE, IN `endDate` DATE)
     NO SQL
 BEGIN
-SELECT Users.name, sum(order_contents.number*products.price) as "total amount"
-FROM order_contents, products, orders, Users
+SELECT users.userName, sum(order_contents.number*products.price) as "total amount"
+FROM order_contents, products, orders, users
 WHERE orders.OID=order_contents.oc_OID
 AND products.PID=order_contents.oc_PID
-AND Users.UID=orders.o_UID
+AND users.UID=orders.o_UID
 
 AND date(orders.date)>=startDate
 AND date(orders.date)<=endDate
-Group BY Users.UID;
+Group BY users.UID;
 END$$
 
 CREATE DEFINER=`mhassan`@`localhost` PROCEDURE `home_orders`()
     NO SQL
 BEGIN
-SELECT orders.date, Users.name, orders.room, Users.extension, sum(order_contents.number*products.price) as "total amount"
-FROM order_contents, products, orders, Users
+SELECT orders.date, users.userName, orders.room, users.extension, sum(order_contents.number*products.price) as "total amount"
+FROM order_contents, products, orders, users
 WHERE orders.OID=order_contents.oc_OID
 AND products.PID=order_contents.oc_PID
-AND Users.UID=orders.o_UID
+AND users.UID=orders.o_UID
 AND orders.status='processing'
 
 Group BY orders.OID;
@@ -54,10 +54,10 @@ END$$
 CREATE DEFINER=`mhassan`@`localhost` PROCEDURE `my_orders`(IN `userId` INT, IN `startDate` DATE, IN `endDate` DATE)
 BEGIN
 SELECT orders.date, orders.status, sum(order_contents.number*products.price) as "total amount"
-FROM order_contents, products, orders, Users
+FROM order_contents, products, orders, users
 WHERE orders.OID=order_contents.oc_OID
 AND products.PID=order_contents.oc_PID
-AND Users.UID=orders.o_UID
+AND users.UID=orders.o_UID
 AND UID=userId
 AND date(orders.date)>=startDate
 AND date(orders.date)<=endDate
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `all_products` (
 --
 CREATE TABLE IF NOT EXISTS `all_users` (
 `name` varchar(50)
-,`room` varchar(50)
+,`room` int(50)
 ,`picture` varchar(255)
 ,`extension` int(11)
 );
@@ -108,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `all_users` (
 CREATE TABLE IF NOT EXISTS `categories` (
   `CID` int(11) NOT NULL,
   `cname` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `categories`
@@ -117,7 +117,10 @@ CREATE TABLE IF NOT EXISTS `categories` (
 INSERT INTO `categories` (`CID`, `cname`) VALUES
 (1, 'Hot Drinks'),
 (2, 'Fresh Juice'),
-(3, 'Soda');
+(3, 'Soda'),
+(4, 'hot'),
+(5, 'jjjj'),
+(6, 'test');
 
 -- --------------------------------------------------------
 
@@ -132,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `status` enum('processing','out for delivery','done') NOT NULL DEFAULT 'processing',
   `room` varchar(50) NOT NULL DEFAULT 'in place',
   `notes` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=778 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=784 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
@@ -145,7 +148,8 @@ INSERT INTO `orders` (`OID`, `o_UID`, `date`, `status`, `room`, `notes`) VALUES
 (444, 4, '2018-01-19 13:47:29', 'processing', 'in place', NULL),
 (555, 1, '2018-01-19 13:47:29', 'processing', 'in place', NULL),
 (666, 1, '2018-01-19 13:47:56', 'processing', 'in place', NULL),
-(777, 2, '2018-01-19 13:47:56', 'processing', 'in place', NULL);
+(777, 2, '2018-01-19 13:47:56', 'processing', 'in place', NULL),
+(778, 3, '2018-01-19 21:06:40', 'processing', 'in place', NULL);
 
 -- --------------------------------------------------------
 
@@ -190,7 +194,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `price` tinyint(4) NOT NULL,
   `picture` varchar(255) NOT NULL DEFAULT '',
   `availability` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
@@ -200,33 +204,45 @@ INSERT INTO `products` (`PID`, `pname`, `p_CID`, `price`, `picture`, `availabili
 (1, 'tea', 1, 5, '', 1),
 (2, 'coffee', 2, 8, '', 1),
 (3, 'orange', 2, 12, '', 1),
-(4, 'Soda', 3, 6, '', 1);
+(4, 'Soda', 3, 6, '', 1),
+(5, 'nescafe', 1, 20, '', 1),
+(11, 'l', 2, 2, 'a', 1),
+(12, 'a', 1, 1, 'a', 1),
+(13, 'a', 1, 1, 'a', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `Users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `UID` int(11) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `room` varchar(50) NOT NULL DEFAULT 'in place',
+  `userName` varchar(50) NOT NULL,
+  `room` int(50) NOT NULL,
   `extension` int(11) DEFAULT NULL,
   `password` char(40) NOT NULL,
-  `picture` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `picture` varchar(255) DEFAULT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `Users` (`UID`, `Email`, `name`, `room`, `extension`, `password`, `picture`) VALUES
-(1, 'mohamed@', 'mohamed', '2010', 2124, '1234', NULL),
-(2, 'ahmed@', 'ahmed', '2005', 12545, '123456', NULL),
-(3, 'mina@', 'mina', '2006', 15485, '123456789', NULL),
-(4, 'omran@', 'omran', '2008', 124587, '124578', NULL);
+INSERT INTO `users` (`UID`, `userName`, `room`, `extension`, `password`, `picture`, `admin`) VALUES
+(1, 'omran11', 0, 0, 'swssd', '', 0),
+(2, 'ahmed@', 2005, 12545, '123456', NULL, 0),
+(3, 'mina@', 2006, 15485, '123456789', NULL, 0),
+(4, 'omran@', 2008, 124587, '124578', NULL, 0),
+(5, 'email', 0, 0, 'password', 'picture', 0),
+(8, 'ahmed', 1515, 0, '125', '', 0),
+(9, 'mohamed', 1515, 0, '125', '', 0),
+(11, 'sds', 0, 0, 'swssd', '', 0),
+(13, 'sfv', 0, 0, 'swssd', '', 0),
+(17, 'ahmeddcd', 0, 0, 'swssd', '', 0),
+(18, 'gemy', 0, 0, 'swssd', '', 0),
+(19, 'omran', 0, 0, 'swssd', '', 0);
 
 -- --------------------------------------------------------
 
@@ -244,7 +260,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`mhassan`@`localhost` SQL SECURITY DEFINER VI
 --
 DROP TABLE IF EXISTS `all_users`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`mhassan`@`localhost` SQL SECURITY DEFINER VIEW `all_users` AS select `Users`.`name` AS `name`,`Users`.`room` AS `room`,`Users`.`picture` AS `picture`,`Users`.`extension` AS `extension` from `Users` WITH CASCADED CHECK OPTION;
+CREATE ALGORITHM=UNDEFINED DEFINER=`mhassan`@`localhost` SQL SECURITY DEFINER VIEW `all_users` AS select `users`.`userName` AS `name`,`users`.`room` AS `room`,`users`.`picture` AS `picture`,`users`.`extension` AS `extension` from `users` WITH CASCADED CHECK OPTION;
 
 --
 -- Indexes for dumped tables
@@ -280,11 +296,12 @@ ALTER TABLE `products`
   ADD KEY `category` (`p_CID`);
 
 --
--- Indexes for table `Users`
+-- Indexes for table `users`
 --
-ALTER TABLE `Users`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`UID`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `Email` (`userName`),
+  ADD KEY `userName` (`userName`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -294,22 +311,22 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `OID` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=778;
+  MODIFY `OID` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=784;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `PID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `PID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
--- AUTO_INCREMENT for table `Users`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `Users`
-  MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+ALTER TABLE `users`
+  MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- Constraints for dumped tables
 --
@@ -318,14 +335,14 @@ ALTER TABLE `Users`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`o_UID`) REFERENCES `Users` (`UID`);
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`o_UID`) REFERENCES `users` (`UID`);
 
 --
 -- Constraints for table `order_contents`
 --
 ALTER TABLE `order_contents`
-  ADD CONSTRAINT `order_contents_ibfk_2` FOREIGN KEY (`oc_PID`) REFERENCES `products` (`PID`),
-  ADD CONSTRAINT `order_contents_ibfk_1` FOREIGN KEY (`oc_OID`) REFERENCES `orders` (`OID`);
+  ADD CONSTRAINT `order_contents_ibfk_1` FOREIGN KEY (`oc_OID`) REFERENCES `orders` (`OID`),
+  ADD CONSTRAINT `order_contents_ibfk_2` FOREIGN KEY (`oc_PID`) REFERENCES `products` (`PID`);
 
 --
 -- Constraints for table `products`
