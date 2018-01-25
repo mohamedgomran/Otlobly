897186b3717c1811129aa7ce35a7a0b7407316da
Mohamed Gamal <m.gemyero91@gmail.com>
2018-01-25 14:51:48 +0200

    modify class order

--- classes/class_order.php
+++ classes/class_order.php
@@ -3,28 +3,50 @@
 	
 	class Order {
 		use dataBase;
-		public $date;
-		public $status;
-		public $room;
-		public $notes;
+		private $id;
+		private $date;
+		private $status;
+		private $room;
+		private $notes;
+	
+	public function __construct($id, $date, $status, $room, $notes) {
+		$this->id = $id;
+		$this->date = $date;
+		$this->status = $status;
+		$this->room = $room;
+		$this->notes = $notes;
+	} 
 
 	public function __get($name) {
 		return $this->$name;
-	} 
+	}
 
 	public function __set($name, $value) {
-		if ($name === "date" && is_string($value) && is_numeric($value)) {
+		if (($name === "id") && is_integer($value)) {
+			$this->id = $value;
+		}
+		else if (($name === "date") && is_string($value) && is_numeric($value)) {
 			$this->date = $value;
 		}
-		else if ($name === "room" && is_integer($value)) {
+		else if (($name === "room") && is_integer($value)) {
 			$this->room = $value;
 		}
-		else if ($name === "status" && ($value === "processing" || $value === "out for delivery" || $value === "done")) {
+		else if (($name === "status") && (($value === "processing") || ($value === "out for delivery") || ($value === "done"))) {
 			$this->status = $value;
 		}
-		else if ($name === "notes" && is_string($value)) {
+		else if (($name === "notes") && is_string($value)) {
 			$this->notes = $value;
 		}
 	}
+
+	public function addOrder($user_id, $order_id, $order_date, $order_status, $order_room, $order_notes) {
+		
+	}
 }
+$o = new Order(10,"25136", "processing", 5, "Hello Gemy");
+echo "$o->id\n";
+echo "$o->date\n";
+echo "$o->status\n";
+echo "$o->room\n";
+echo "$o->notes\n";
 ?>
