c55a4f4ae78d57cd4e6caded11884672dd7caee0
Moahmed Omran <mohamedgomran@gmail.com>
2018-01-25 13:56:14 +0200

    edit

--- classes/class_category.php
+++ classes/class_category.php
@@ -1,6 +1,51 @@
 <?php
 
+// Class category, has attribute of name as string
+// void addCatefory(); add catgoty to database
+
+	include_once 'dbase.php';
+	
+	class category
+	{
+		
+	use dataBase; 
+	private	$name;
 
 
+	public function __construct($Name)
+	{
+		$this->name=$Name;
+	}
+
+	public function __set($param,$value)
+	{
+		$this->$param=$value;
+	}
+	public function __get($param)
+	{
+		return $this->$param;
+	}
+
+	public function addCategory()
+	{
+		$query = "insert into categories values (null, ?)";
+		$dataArr  = array($this -> __get("name"));
+		$this -> manDb($query, $dataArr);
+	}
+
+	static function getCategory()
+	{
+		$query = "select * from categories";
+		$dataArr  = array();
+		$prep = category::manDb($query, $dataArr);
+		$result = $prep->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
+
+}
+
+
+
+print_r(category::getCategory());
 
 ?>
