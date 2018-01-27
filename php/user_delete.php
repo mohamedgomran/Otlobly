be83d3673bce544b1f9e291b13b9aecce4f72ec9
AsSamha <assamhaa@gmail.com>
2018-01-27 02:19:29 +0200

    minor changes

--- php/user_delete.php
+++ php/user_delete.php
@@ -1,16 +1,8 @@
- <?php
- 	include_once('../classes/class_user.php');
+<?php
+	// needs to check for the identity of the sender
+	include_once('../classes/class_user.php');
+	
+	user::removeUser($_POST['userID']);
+	json_encode($_POST['userID']);
 
- 	// $id_of_product_to_be_deleted = $_GET['id'];
-
- 	// needs to check for the identity of the sender
-
-
- 	$_POST['userID'] = 2;
- 	// echo $_POST['userID'];
-
- 	user::removeUser($_POST['userID']);
-
- 	// json_encode(Product::get_available_products());
-
- ?>
+?>
