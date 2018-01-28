9721424d661d7d697d09307cc391a72ddd19947c
Mohamed Gamal <m.gemyero91@gmail.com>
2018-01-28 15:35:52 +0200

    fix another bugs

--- php/add_category.php
+++ php/add_category.php
@@ -1,19 +1,19 @@
 <?php
 
-    include_once "../classes/class_order.php";
+    include_once "../classes/class_category.php";
 
     
     $success = true;
 
     $query = "select cname from categories where cname = ?";
     $dataArr  = array($_REQUEST["category"]);
-    $prep = Order::manDb($query, $dataArr);
+    $prep = category::manDb($query, $dataArr);
     $result = $prep->fetchAll(PDO::FETCH_ASSOC);
 
     if (empty($result)) {
         $query1 = "insert into categories values(null, ?)";
         $dataArr1  = array($_REQUEST["category"]);
-        Order::manDb($query1, $dataArr1);
+        category::manDb($query1, $dataArr1);
         $success = true;
     } else {
         $success = false;
